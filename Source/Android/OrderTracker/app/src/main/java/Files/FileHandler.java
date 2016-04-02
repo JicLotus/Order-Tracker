0d927b0cc159e9bca5af7d9c85a52dac87c7d03c
jiclotus <jose-castelli@hotmail.com>
2016-04-02 13:14:37 -0300

    Subo clase que sirve para decargar imagenes, falta vincularlas y listo.

---
+++ Source/Android/OrderTracker/app/src/main/java/Files/FileHandler.java
@@ -0,0 +1,45 @@
+package Files;
+
+import android.util.Base64;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import Model.Request;
+import Model.RequestHandler;
+import Model.Response;
+
+/**
+ * Created by lotus on 2/04/16.
+ */
+public class FileHandler {
+
+    public FileHandler()
+    {
+    }
+
+    public void downloadFile(String path,String indentifierFile)
+    {
+        try {
+            File file = new File(path);
+            Request request = new Request("GET", "GetImagen.php?id=" + indentifierFile);
+            Response resp = new RequestHandler().sendRequest(request);
+
+            saveFile(file,resp.getJsonArray().getJSONObject(0).getString("imagen"));
+        }
+        catch(Exception e)
+        {
+        }
+    }
+
+    private void saveFile(File file,String textoBytes) throws IOException {
+        byte[] bytes = Base64.decode(textoBytes, Base64.DEFAULT);
+        FileOutputStream fop = new FileOutputStream(file);
+        fop.write(bytes);
+        fop.flush();
+        fop.close();
+    }
+
+
+}
