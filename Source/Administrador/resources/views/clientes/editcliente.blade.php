e41168f3404deb086f4483d0d1290289de5e211a
Juan Andres Laura <moncholo06@gmail.com>
2016-04-23 13:00:13 -0300

    Imagen QR en Editar Cliente

--- Source/Administrador/resources/views/clientes/editcliente.blade.php
+++ Source/Administrador/resources/views/clientes/editcliente.blade.php
@@ -10,67 +10,76 @@
 </div>
 
 <div class="row">
-  <div class="col-md-12">
-    
  
-<form action="guardarcliente" method="POST" class="form-horizontal" enctype="multipart/form-data">
-	{{ csrf_field() }}
-	<input type="hidden" name="idCliente" value="{{$cliente->id}}">
-	
-	<div class="form-group">
-		<label class="control-label col-sm-2" for="nombre">Nombre</label>
-          <div class="col-sm-8">
-			<input type="Text" name="nombre" value="{{$cliente->nombre}}" ><br>
-		  </div>
-    </div>
-    
-    <div class="form-group">
-		<label class="control-label col-sm-2" for="email">Email</label>
-          <div class="col-sm-8">
-			<input type="Text" name="email" value="{{$cliente->email}}" ><br>
-		  </div>
-    </div>
-    
-    <div class="form-group">
-		<label class="control-label col-sm-2" for="direccion">Dirección</label>
-          <div class="col-sm-8">
-			<input type="Text" name="direccion" value="{{$cliente->direccion}}"> <br>
-		  </div>
-    </div>
-    
-     <div class="form-group">
-		<label class="control-label col-sm-2" for="razon_social">Razón Social</label>
-          <div class="col-sm-8">
-			<input type="Text" name="razon_social"  value="{{$cliente->razon_social}}"><br>
-		  </div>
-    </div>	
-    
-    <div class="form-group">
-		<label class="control-label col-sm-2" for="telefono_movil">Teléfono Móvil</label>
-          <div class="col-sm-8">
-			<input type="Text" name="telefono_movil" value="{{$cliente->telefono_movil}}"><br>
-		  </div>
-    </div>
-    
-    <div class="form-group">
-		<label class="control-label col-sm-2" for="telefono_laboral">Teléfono Laboral</label>
-          <div class="col-sm-8">
-			<input type="Text" name="telefono_laboral"  value="{{$cliente->telefono_laboral}}"><br>
-		  </div>
-    </div>
-    
-        <div class="form-group">
-            <div class="col-sm-offset-3 col-sm-8">
-                <a href="{{ URL::previous() }}" class="btn btn-default">Cancelar</a>
+<script type="text/javascript" src="{{ URL::asset('js/qrcode.js') }}"></script>
+	<div class="col-md-4" id="qr">
+			<script>document.getElementById('qr').innerHTML=create_qrcode("{{$cliente->id}}");
+			</script>
+	</div>
 
-                <button type="submit" class="col-sm-offset-5 btn btn-default">Publicar
-                </button>
+  <div class="col-md-8">
+     
+	<form action="mailto:moncholo06@gmail.com" method="POST" class="form-horizontal" enctype="multipart/form-data">
+		{{ csrf_field() }}
+		<input type="hidden" name="idCliente" value="{{$cliente->id}}">
+		
+		
+		<div class="form-group">
+			<label class="control-label col-sm-2" for="nombre">Nombre</label>
+			  <div class="col-sm-8">
+				<input type="Text" name="nombre" value="{{$cliente->nombre}}" ><br>
+			  </div>
+		</div>
+		
+		<div class="form-group">
+			<label class="control-label col-sm-2" for="email">Email</label>
+			  <div class="col-sm-8">
+				<input type="Text" name="email" value="{{$cliente->email}}" ><br>
+			  </div>
+		</div>
+		
+		<div class="form-group">
+			<label class="control-label col-sm-2" for="direccion">Dirección</label>
+			  <div class="col-sm-8">
+				<input type="Text" name="direccion" value="{{$cliente->direccion}}"> <br>
+			  </div>
+		</div>
+		
+		 <div class="form-group">
+			<label class="control-label col-sm-2" for="razon_social">Razón Social</label>
+			  <div class="col-sm-8">
+				<input type="Text" name="razon_social"  value="{{$cliente->razon_social}}"><br>
+			  </div>
+		</div>	
+		
+		<div class="form-group">
+			<label class="control-label col-sm-2" for="telefono_movil">Teléfono Móvil</label>
+			  <div class="col-sm-8">
+				<input type="Text" name="telefono_movil" value="{{$cliente->telefono_movil}}"><br>
+			  </div>
+		</div>
+		
+		<div class="form-group">
+			<label class="control-label col-sm-2" for="telefono_laboral">Teléfono Laboral</label>
+			  <div class="col-sm-8">
+				<input type="Text" name="telefono_laboral"  value="{{$cliente->telefono_laboral}}"><br>
+			  </div>
+		</div>
 
-            </div>
-        </div> 		
-			
-	
-</form>
+
+			<div class="form-group">
+				<div class="col-sm-8">
+					<a href="{{ URL::previous() }}" class="btn btn-default">Cancelar</a>
+
+					<button type="submit" class="col-sm-offset-4 btn btn-default">Publicar
+					</button>
+
+				</div>
+			</div> 		
+					
+	</form>
+
+
                               
   </div>
 </div>
