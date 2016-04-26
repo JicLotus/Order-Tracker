d3630b6d45a6537e34de90b2a49ca6a6fa7b29dc
Juan Andres Laura <moncholo06@gmail.com>
2016-04-26 14:50:14 -0300

    Persistencia de fechas en la agenda

--- Source/Administrador/app/Http/Controllers/NuevaAgendaController.php
+++ Source/Administrador/app/Http/Controllers/NuevaAgendaController.php
@@ -9,8 +9,9 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Redirect;
 use File;
-
-	 
+use \DateTime;
+use \DatePeriod;
+use \DateInterval;
 
 
 class NuevaAgendaController extends Controller
@@ -31,18 +32,49 @@ class NuevaAgendaController extends Controller
     {
 		$idVendedor= $request->idVendedor;    	
 
-    	$hora = "00:00";
-    	
-    	$old_date = date("11-11-2016". $hora);           
+    	$dia = $request->dia;
+		
+		$dt = new DateTime();
+		// set object to Monday on next week
+
+		$dt->setISODate($dt->format('o'), $dt->format('W') + 1);
+
+		// get all 1day periods from Monday to +6 days
+		$periods = new DatePeriod($dt, new DateInterval('P1D'), 6);
+		
+		$days = iterator_to_array($periods);
+		// convert DatePeriod object to array
+
+		// $days[0] is Monday, ..., $days[6] is Sunday
+		// to format selected date do: $days[1]->format('Y-m-d');
+		switch ($dia){
+			case "Lunes":
+			    	$old_date = $days[0]->format ('Y-m-d');
+			    	break;
+			case "Martes":
+			    	$old_date = $days[1]->format ('Y-m-d');
+					break;
+			case "Miercoles":
+			    	$old_date = $days[2]->format ('Y-m-d');
+					break;
+			case "Jueves":
+			    	$old_date = $days[3]->format ('Y-m-d');
+					break;
+			case "Viernes":
+			    	$old_date = $days[4]->format ('Y-m-d');
+					break;
+		}
+	
 		$middle = strtotime($old_date);             
+		
 		$fecha = date('Y-m-d H:i:s', $middle); 
 		
     	$idCliente= $request->idCliente[0];
-    	
+
     	foreach ($request->idCliente as $cliente){
 			$id = DB::table('agendas')->insertGetId(array('id_usuario' => ($idVendedor),'id_cliente' => ($cliente), 'fecha' => ($fecha), 'dia' => $request->dia));
 		}
-		
+			echo $dia;
 		$url = app()->make('urls')->getUrlAgendas();
 		return redirect($url);
     }
